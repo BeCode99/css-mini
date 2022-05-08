fbe96b3d1a511887506229d80ca2580bed57690f
BeCode99 <89093308+BeCode99@users.noreply.github.com>
2022-05-09 00:37:31 +0700

    mini-4

--- 4-shutter-effects/shutter.js
+++ 4-shutter-effects/shutter.js
@@ -1,5 +1,13 @@
 let valueShutter = document.querySelector('.valueShutter');
-
+let filterLeftTop = document.querySelector('.filterLeftTop')
+let filterRightTop = document.querySelector('.filterRightTop')
+let filterRightBottom = document.querySelector('.filterRightBottom')
+let filterLeftBottom = document.querySelector('.filterLeftBottom')
 function changeValue(value) {
     valueShutter.innerHTML = value;
+    filterLeftTop.style.width = value/2 + "%";
+    filterRightTop.style.height = value/2 + "%";
+    filterRightBottom.style.width = value/2 + "%";
+    filterLeftBottom.style.height = value/2 + "%";
+    
 }
